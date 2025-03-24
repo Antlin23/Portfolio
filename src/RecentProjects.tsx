7d466ad14eec5533ca808e66b1a143b29a9b20bb
Antlin23 <anton.jumkil@gmail.com>
2025-03-24 18:26:27 +0100

    drift project added

--- src/RecentProjects.tsx
+++ src/RecentProjects.tsx
@@ -5,6 +5,7 @@ import kulturnattenImg from './images/kulturnatten.png'
 import adventOfCodeIMG from './images/adventOfCodeIMG.png'
 import flutterNamerImg from './images/flutterNamer.png'
 import fixoImg from './images/fixo.png'
+import ovldcars from './images/ovldcars.jpg'
 //tool icons
 import flutterLogo from './images/flutterLogo.png'
 import dartLogo from './images/dartLogo.png'
@@ -18,6 +19,7 @@ import cSharpLogo from './images/Csharp_Logo.png'
 import swishLogo from './images/swishLogo.png'
 import sendGridLogo from './images/sendgridLogo.png'
 import mssqlLogo from './images/mssqlLogo.png'
+import luaLogo from './images/lua.png'
 
 const RecentProjects = () => {
   var kulturnattenToolLogos: Array<{ img: string; text: string }> = [];
@@ -29,9 +31,8 @@ const RecentProjects = () => {
   var aocToolLogos: Array<{ img: string; text: string }> = [];
   aocToolLogos.push({ img: cSharpLogo, text: "C#" });
 
-  var wordGeneratorToolLogos: Array<{ img: string; text: string }> = [];
-  wordGeneratorToolLogos.push({ img: flutterLogo, text: "Flutter" });
-  wordGeneratorToolLogos.push({ img: dartLogo, text: "Dart" });
+  var driftMenuToolLogos: Array<{ img: string; text: string }> = [];
+  driftMenuToolLogos.push({ img: luaLogo, text: "Lua" });
 
   var fixoToolLogos: Array<{ img: string; text: string }> = [];
   fixoToolLogos.push({ img: netLogo, text: ".NET" });
@@ -53,6 +54,18 @@ const RecentProjects = () => {
         />
 
         <Project 
+          Title='FiveM mod menu' 
+          Description='Mod menu (hack) for FiveM (GTA) with vehicle drift tune, counter & more made with Lua.' 
+          LinkSvg={
+            <svg viewBox="0 0 16 16" width="22" height="22" aria-hidden="true"><path fillRule="evenodd" fill="currentColor" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+          } 
+          Link='https://github.com/Antlin23/wordGenerator'
+          LinkText="See on GitHub"
+          Image={ovldcars}
+          ToolIcons={driftMenuToolLogos}
+        />
+
+        <Project 
           Title='kulturnatten.uppsala.se'
           Description='I was part of the dev team from start to finish'
           LinkSvg={
@@ -62,18 +75,6 @@ const RecentProjects = () => {
           LinkText="View site"
           Image={kulturnattenImg}
           ToolIcons={kulturnattenToolLogos}
-        />
-
-        <Project 
-          Title='Word generator' 
-          Description='Random word generator' 
-          LinkSvg={
-            <svg viewBox="0 0 16 16" width="22" height="22" aria-hidden="true"><path fillRule="evenodd" fill="currentColor" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8z"></path></svg>
-          } 
-          Link='https://github.com/Antlin23/wordGenerator'
-          LinkText="See on GitHub"
-          Image={flutterNamerImg}
-          ToolIcons={wordGeneratorToolLogos}
         />
 
         <Project 
@@ -90,5 +91,19 @@ const RecentProjects = () => {
       </>
   );
 }
+
+/*
+        <Project 
+          Title='Word generator' 
+          Description='Random word generator' 
+          LinkSvg={
+            <svg viewBox="0 0 16 16" width="22" height="22" aria-hidden="true"><path fillRule="evenodd" fill="currentColor" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8z"></path></svg>
+          } 
+          Link='https://github.com/Antlin23/wordGenerator'
+          LinkText="See on GitHub"
+          Image={flutterNamerImg}
+          ToolIcons={wordGeneratorToolLogos}
+        />
+*/
 
 export default RecentProjects;
